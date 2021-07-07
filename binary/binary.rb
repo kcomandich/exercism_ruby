f29e18e49f0195fa06b836d7019359727ff0dc95
Kirsten Comandich <kcomandich@gmail.com>
2021-07-07 14:48:35 +1000

    Binary: use a regex to test for valid input (instead of comparing characters). From another user's submitted solution: https://exercism.io/tracks/ruby/exercises/binary/solutions/b32ee6e011bb4305b03c4cafcb5b4734

--- binary/binary.rb
+++ binary/binary.rb
@@ -7,14 +7,14 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 module Binary
- def self.to_decimal(binary)
-   result = 0
-   place = 1
+  def self.to_decimal(binary)
+    raise ArgumentError if binary =~ /[^01]/
+    result = 0
+    place = 1
     binary.reverse.each_char do |digit|
-     raise(ArgumentError) if digit != '0' and digit != '1'
      result += place * digit.to_i
      place *= 2
     end
    return result
- end
+  end
 end
