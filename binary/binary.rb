d7d48b0cf4d7cec638401104bcd38c9c5062f652
Kirsten Comandich <kcomandich@gmail.com>
2021-07-07 15:23:03 +1000

    Binary: not sure why I used a module instead of a class.

--- binary/binary.rb
+++ binary/binary.rb
@@ -6,7 +6,7 @@ To get started with TDD, see the `README.md` file in your
 `ruby/binary` directory.
 =end
 
-module Binary
+class Binary
   def self.to_decimal(binary)
     raise ArgumentError if binary =~ /[^01]/
     result = 0
