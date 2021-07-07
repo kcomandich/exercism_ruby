ec266bed587e7c80da4148366c5f75275ceed675
Kirsten Comandich <kcomandich@gmail.com>
2021-07-07 14:13:48 +1000

    Leap: solution mostly completed in May 2014, cleaned up to pass all tests in 2021.

---
+++ leap/year.rb
@@ -0,0 +1,31 @@
+class Year
+  def self.leap?(year)
+    if divisible_by_400(year)
+      return true
+    elsif divisible_by_four(year) && !divisible_by_100(year)
+      return true
+    else
+      return false
+    end
+  end
+
+  private
+
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def self.divisible_by_four(year)
+    year%4 == 0
+  end
+
+  def self.divisible_by_100(year)
+    year%100 == 0
+  end
+
+  def self.divisible_by_400(year)
+    year%400 == 0
+  end
+end
