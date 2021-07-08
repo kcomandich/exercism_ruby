3996093c7817519365622918ee9622e4811e2ec8
Kirsten Comandich <kcomandich@gmail.com>
2021-07-08 22:20:09 +1000

    Resistor Color Duo: first solution.

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,6 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
-
+class ResistorColorDuo
+  COLORS = [ "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  def self.value(colors)
+    COLORS.index(colors[0]) * 10 + COLORS.index(colors[1])
+  end
+end
