668965afde6c5973c465254a3fe955c1f8937500
Kirsten Comandich <kcomandich@gmail.com>
2021-08-06 17:49:46 +1000

    Hamming: don't need to use a counter variable and the ternary operator inside the zip block - can just use the count method with a simpler block.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,10 +1,7 @@
 class Hamming
   def self.compute(seq1, seq2)
-
     raise ArgumentError.new("Sequences must be the same length") if seq1.size != seq2.size
 
-    result = 0
-    seq1.each_char.zip(seq2.each_char) {|sub_array| result += sub_array[0] == sub_array[1] ? 0 : 1 }
-    result
+    seq1.each_char.zip(seq2.each_char).count {|a| a[0] != a[1] }
   end
 end
