4fb16c12b31216a274de504d075eb9a091e7d079
Kirsten Comandich <kcomandich@gmail.com>
2021-08-06 16:46:40 +1000

    Hamming: another solution - but longer code that uses the zip method - does not make anything clearer.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,9 +1,10 @@
 class Hamming
   def self.compute(seq1, seq2)
+
     raise ArgumentError.new("Sequences must be the same length") if seq1.size != seq2.size
 
     result = 0
-    seq1.each_char.with_index {|char, i| result += char == seq2[i] ? 0 : 1 }
+    seq1.each_char.zip(seq2.each_char) {|sub_array| result += sub_array[0] == sub_array[1] ? 0 : 1 }
     result
   end
 end
