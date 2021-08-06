ea4e88edebcc796022f74526f25b1bc44e1c4cb5
Kirsten Comandich <kcomandich@gmail.com>
2021-08-06 18:11:01 +1000

    Hamming: replace zip method because I can just use each_char.with_index to loop through the first sequence and compare the second sequence inside the block. I think this is easier to read because you don't have to think about what the zip method returns.

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,6 +2,6 @@ class Hamming
   def self.compute(seq1, seq2)
     raise ArgumentError.new("Sequences must be the same length") if seq1.size != seq2.size
 
-    seq1.each_char.zip(seq2.each_char).count {|a| a[0] != a[1] }
+    seq1.each_char.with_index.count {|char, i| char != seq2[i] }
   end
 end
