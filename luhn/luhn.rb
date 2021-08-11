774c5485113ba8c0152815965d3b92cf8ff5e3b8
Kirsten Comandich <kcomandich@gmail.com>
2021-08-11 17:25:11 +1000

    Luhn: add invalid_input? method to pull out that logic and clean up the main method.

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -2,8 +2,7 @@ class Luhn
 
   def self.valid?(number)
     number.gsub!(/\s/, '')
-    return false if number.match(/\D/)
-    return false if number.size <= 1
+    return false if invalid_input?(number)
 
     result = number.reverse.each_char.with_index.map{ |n, i| i.odd? ? double_a_digit(n.to_i) : n }
     result = result.reduce(0) {|sum, n| sum + n.to_i}
@@ -13,6 +12,10 @@ class Luhn
   end
 
   private
+
+  def self.invalid_input?(number)
+    number.size <= 1 or number.match(/\D/)
+  end
 
   def self.double_a_digit(digit)
     digit = digit * 2
