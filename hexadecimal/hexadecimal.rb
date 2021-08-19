de168b0c1e4a6367a7fed80ee40af28fd8662252
Kirsten Comandich <kcomandich@gmail.com>
2021-08-19 17:49:23 +1000

    Hexadecimal: revert to non-recursive solution - and then simplify by using the index of '0123456789abcdef' to calculate digit's value, rather than a hash of values and the ternary conditional. Suggested by another's exercism solution. Could also use     return 0 if @hexadecimal.match(Regexp.new("[^#{HEX_DIGITS}]")) to cut down the number of lines further, but I'm not sure that adds to readability.

--- hexadecimal/hexadecimal.rb
+++ hexadecimal/hexadecimal.rb
@@ -1,7 +1,6 @@
 class Hexadecimal
   INVALID_INPUT = /[^\h]/.freeze
-  ALPHA = /[abcdef]/.freeze
-  HEX_MAP = { 'a' =>  10, 'b' =>  11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15 }.freeze
+  HEX_DIGITS = '0123456789abcdef'.freeze
 
   def initialize(hexadecimal)
     @hexadecimal = hexadecimal
@@ -9,17 +8,6 @@ class Hexadecimal
 
   def to_decimal
     return 0 if @hexadecimal =~ INVALID_INPUT
-    dec(@hexadecimal)
-  end
-
-  private
-
-  def dec(chars)
-    return 0 if chars == ''
-    dec(chars[..-2])*16 + digit_value(chars[-1])
-  end
-
-  def digit_value(char)
-    char =~ ALPHA ?  HEX_MAP[char] : char.to_i
+    @hexadecimal.chars.reduce(0) {|sum, c| sum*16 + HEX_DIGITS.index(c) }
   end
 end
