3a7059ec602e287dadaea063e6431be2bb24d76d
Kirsten Comandich <kcomandich@gmail.com>
2021-08-19 16:37:26 +1000

    Hexadecimal: first code that passes all the tests. Heavily influenced by my trinary solution as I just finished that one earlier today.

---
+++ hexadecimal/hexadecimal.rb
@@ -0,0 +1,14 @@
+class Hexadecimal
+  INVALID_INPUT = /[^\h]/.freeze
+  ALPHA = /[abcdef]/.freeze
+  HEX_MAP = { 'a' =>  10, 'b' =>  11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15 }.freeze
+
+  def initialize(hexadecimal)
+    @hexadecimal = hexadecimal
+  end
+
+  def to_decimal
+    return 0 if @hexadecimal =~ INVALID_INPUT
+    @hexadecimal.chars.reduce(0) {|sum, c| sum*16 + (c =~ ALPHA ? HEX_MAP[c] : c.to_i) }
+  end
+end
