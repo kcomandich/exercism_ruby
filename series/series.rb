d314535d1cb80d11dea41886d42e804e4acb83ad
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 07:52:44 +1000

    Series: use an attr_reader.

--- series/series.rb
+++ series/series.rb
@@ -1,10 +1,12 @@
 class Series
+  attr_reader :series
+
   def initialize(string)
     @series = string.split('')
   end
 
   def slices(count)
     raise ArgumentError if count > @series.size
-    @series.each_cons(count).map(&:join)
+    series.each_cons(count).map(&:join)
   end
 end
