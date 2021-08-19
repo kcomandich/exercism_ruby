f2dac7aa3f383f628e4fef758746d308e4613d56
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 08:36:59 +1000

    Binary: remove boilerplate comments at the top of file.

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,11 +1,3 @@
-=begin
-Write your code for the 'Binary' exercise in this file. Make the tests in
-`binary_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/binary` directory.
-=end
-
 class Binary
   def self.to_decimal(binary)
     raise ArgumentError if binary =~ /[^01]/
