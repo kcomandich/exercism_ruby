3ac532cdace6ca8ed1e75eed5a050c7d063f44fb
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 08:35:02 +1000

    Hexadecimal: use an attr_reader.

--- hexadecimal/hexadecimal.rb
+++ hexadecimal/hexadecimal.rb
@@ -2,12 +2,14 @@ class Hexadecimal
   INVALID_INPUT = /[^\h]/.freeze
   HEX_DIGITS = '0123456789abcdef'.freeze
 
+  attr_reader :hexadecimal
+
   def initialize(hexadecimal)
     @hexadecimal = hexadecimal
   end
 
   def to_decimal
-    return 0 if @hexadecimal =~ INVALID_INPUT
-    @hexadecimal.chars.reduce(0) {|sum, c| sum*16 + HEX_DIGITS.index(c) }
+    return 0 if hexadecimal =~ INVALID_INPUT
+    hexadecimal.chars.reduce(0) {|sum, c| sum*16 + HEX_DIGITS.index(c) }
   end
 end
