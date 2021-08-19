eaf999cfc2aff6e0fce9479ff130065e967ca846
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 08:43:47 +1000

    Binary: use "reduce" method instead of an accumulator variable. Also use the trick I learned with Trinary for multiplying each successive sum by 2, rather than reversing the string to do the calculation.

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,8 +1,6 @@
 class Binary
   def self.to_decimal(binary)
     raise ArgumentError if binary =~ /[^01]/
-    result = 0
-    binary.reverse.each_char.with_index {|char, index| result += char.to_i * 2**index }
-    return result
+    binary.chars.reduce(0) {|sum, char| sum*2 + char.to_i }
   end
 end
