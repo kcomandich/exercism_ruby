db9699d9eb4dcc3f1665a939460a31e10dc3d69a
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 07:57:00 +1000

    Word Count: use an attr_reader.

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,11 +1,12 @@
 class Phrase
   WORD = /\b[\w']+\b/.freeze
+  attr_reader :words
 
   def initialize(phrase)
     @words = phrase.scan(WORD).map(&:downcase)
   end
 
   def word_count
-    @words.to_h{|word| [word, @words.count(word)] }
+    words.to_h{|word| [word, words.count(word)] }
   end
 end
