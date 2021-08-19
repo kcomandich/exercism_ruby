32388007acb984994770dd5bbe094a3797b778d2
Kirsten Comandich <kcomandich@gmail.com>
2021-08-20 08:31:56 +1000

    Trinary: use an attr_reader.

--- trinary/trinary.rb
+++ trinary/trinary.rb
@@ -1,12 +1,14 @@
 class Trinary
   INVALID_INPUT = /[^012]/.freeze
 
+  attr_reader :trinary
+
   def initialize(trinary)
     @trinary = trinary
   end
 
   def to_decimal
-    return 0 if @trinary =~ INVALID_INPUT
-    @trinary.chars.reduce(0) { |sum, c| 3*sum + c.to_i }
+    return 0 if trinary =~ INVALID_INPUT
+    trinary.chars.reduce(0) { |sum, c| 3*sum + c.to_i }
   end
 end
