e979cf77a1384684de45d415394b0316cdafc076
Kirsten Comandich <kcomandich@gmail.com>
2021-08-24 17:50:04 +1000

    Microwave: ... and in fact the first conditional of the case statement can be removed all together. divmod(100) returns zero minutes when the input is less than 100, and the minutes gets calculated in the next line where we divide by 60.

--- microwave/microwave.rb
+++ microwave/microwave.rb
@@ -2,14 +2,9 @@ class Microwave
   attr_reader :minutes, :seconds
 
   def initialize(input)
-    case input
-      when 0..99
-        @minutes, @seconds = input.divmod(60)
-      else
-        @minutes, @seconds = input.divmod(100)
-        add_min, @seconds = @seconds.divmod(60)
-        @minutes += add_min
-      end
+    @minutes, @seconds = input.divmod(100)
+    add_min, @seconds = @seconds.divmod(60)
+    @minutes += add_min
   end
 
   def timer
