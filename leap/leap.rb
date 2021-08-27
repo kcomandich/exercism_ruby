0088712f6021d6a51344012bea9ac28a03540363
Kirsten Comandich <kcomandich@gmail.com>
2021-08-27 16:50:13 +1000

    Leap: the helper methods are so simple that it might just be more readable to have the code inline. Idea from kerrizor's exercism solution.

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,31 +1,15 @@
 class Year
-  attr_reader :year
-
-  def initialize(year)
-    @year = year
-  end
-
-  def divisible_by_four?
-    year % 4 == 0
-  end
-
-  def divisible_by_100?
-    year % 100 == 0
-  end
-
-  def divisible_by_400?
-    year % 400 == 0
-  end
-
   def self.leap?(year)
-    check_year = Year.new(year)
-
-    if check_year.divisible_by_400?
+    if (year % 400 == 0)
       return true
-    elsif check_year.divisible_by_four? && ! check_year.divisible_by_100?
+    elsif (year % 4 == 0) && !(year % 100 == 0)
       return true
     else
       return false
     end
   end
+
+  #use refinements?
+  #use zero? method?
+
 end
