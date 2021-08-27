c913beaa969aa7ba412c4375e3354c89328f006e
Kirsten Comandich <kcomandich@gmail.com>
2021-08-27 17:08:33 +1000

    Leap: create a divisible? instance method and use it to make the main leap? method slightly more readable. (Better than the instance methods I had before because this is just one generic one I can use for all divisors.

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,11 +1,23 @@
 class Year
-  def self.leap?(year)
-    if (year % 400).zero?
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+
+  def self.leap?(input)
+    year = Year.new(input)
+
+    if year.divisible?(400)
       return true
-    elsif (year % 4).zero? && !(year % 100).zero?
+    elsif year.divisible?(4) && ! year.divisible?(100)
       return true
     else
       return false
     end
+  end
+
+  def divisible?(divisor)
+    (year % divisor).zero?
   end
 end
