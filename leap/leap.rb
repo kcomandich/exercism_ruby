9095d42db447e95fbf1ce167255bcfe4162da098
Kirsten Comandich <kcomandich@gmail.com>
2021-08-27 16:53:05 +1000

    Leap: use the zero? method rather than comparing to 0. From another exercism solution.

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,15 +1,11 @@
 class Year
   def self.leap?(year)
-    if (year % 400 == 0)
+    if (year % 400).zero?
       return true
-    elsif (year % 4 == 0) && !(year % 100 == 0)
+    elsif (year % 4).zero? && !(year % 100).zero?
       return true
     else
       return false
     end
   end
-
-  #use refinements?
-  #use zero? method?
-
 end
