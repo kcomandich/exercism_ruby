abaec1e246702613cf341c7222c53212380654d3
Kirsten Comandich <kcomandich@gmail.com>
2021-08-27 17:20:07 +1000

    Leap: simplify the conditional into one line.

--- leap/leap.rb
+++ leap/leap.rb
@@ -7,14 +7,7 @@ class Year
 
   def self.leap?(input)
     year = Year.new(input)
-
-    if year.divisible?(400)
-      return true
-    elsif year.divisible?(4) && ! year.divisible?(100)
-      return true
-    else
-      return false
-    end
+    year.divisible?(400) || year.divisible?(4) && ! year.divisible?(100)
   end
 
   def divisible?(divisor)
