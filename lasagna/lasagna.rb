87ef37b712ed5bb43ea1cb5a33c2bcbbf0fa024d
Kirsten <kcomandich@gmail.com>
2021-09-02 14:26:48 +1000

    Lasagna: slight improvement suggested by community solutions - use a constant for the number of minutes of prep per layer, rather than hardcoding it.

--- lasagna/lasagna.rb
+++ lasagna/lasagna.rb
@@ -1,12 +1,13 @@
 class Lasagna
   EXPECTED_MINUTES_IN_OVEN = 40.freeze
+  PREP_MINUTES_PER_LAYER = 2.freeze
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
     EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    layers * 2
+    layers * PREP_MINUTES_PER_LAYER
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
