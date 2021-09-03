9bc1cac755cede7cf069f9f91dfcd868358a2c32
Kirsten <kcomandich@gmail.com>
2021-09-03 14:42:20 +1000

    Series: mentor suggestion - encapsulate Series#series so it's not accessible publicly.

--- series/series.rb
+++ series/series.rb
@@ -1,6 +1,4 @@
 class Series
-  attr_reader :series
-
   def initialize(digit_string)
     @series = digit_string.chars
   end
@@ -8,5 +6,11 @@ class Series
   def slices(count)
     raise ArgumentError if count > @series.size
     series.each_cons(count).map(&:join)
+  end
+
+  private
+
+  def series
+    @series
   end
 end
