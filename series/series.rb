2ef8aae10e6f75381d628f84395332dc47007f28
Kirsten <kcomandich@gmail.com>
2021-09-04 15:19:21 +1000

    Series: mentor suggestion - I didn't need to remove the attr_reader, just make it private and the rest of the methods public.

--- series/series.rb
+++ series/series.rb
@@ -1,4 +1,11 @@
 class Series
+
+  private
+
+  attr_reader :series
+
+  public
+
   def initialize(digit_string)
     @series = digit_string.each_char
   end
@@ -6,11 +13,5 @@ class Series
   def slices(count)
     raise ArgumentError if count > @series.size
     series.each_cons(count).map(&:join)
-  end
-
-  private
-
-  def series
-    @series
   end
 end
