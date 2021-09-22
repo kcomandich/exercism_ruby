1d4cf07a05addbefaba2ef2e971e022a6ae57af3
Kirsten <kcomandich@gmail.com>
2021-09-22 17:59:18 +1000

    Scrabble Score: mentor tip - @values can be a constant, rather than creating it every time. Rename it to TILES because it's not just values, it's also the letters.

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,26 +1,27 @@
 class Scrabble
   private
-  attr_reader :letters, :values
+  attr_reader :letters
+
+  TILES = {
+    'A' => 1, 'B' => 3, 'C' => 3, 
+    'D' => 2, 'E' => 1, 'F' => 4, 
+    'G' => 2, 'H' => 4, 'I' => 1, 
+    'J' => 8, 'K' => 5, 'L' => 1, 
+    'M' => 3, 'N' => 1, 'O' => 1, 
+    'P' => 3, 'Q' => 10, 'R' => 1, 
+    'S' => 1, 'T' => 1, 'U' => 1, 
+    'V' => 4, 'W' => 4, 'X' => 8, 
+    'Y' => 4, 'Z' => 10,
+  }.freeze
 
   public
   def initialize(letters)
     @letters = letters.to_s.strip.upcase.chars
-    @values = {
-      'A' => 1, 'B' => 3, 'C' => 3, 
-      'D' => 2, 'E' => 1, 'F' => 4, 
-      'G' => 2, 'H' => 4, 'I' => 1, 
-      'J' => 8, 'K' => 5, 'L' => 1, 
-      'M' => 3, 'N' => 1, 'O' => 1, 
-      'P' => 3, 'Q' => 10, 'R' => 1, 
-      'S' => 1, 'T' => 1, 'U' => 1, 
-      'V' => 4, 'W' => 4, 'X' => 8, 
-      'Y' => 4, 'Z' => 10,
-    }
   end
 
   def score
     return 0 unless letters
-    letters.sum {|l| values[l] }
+    letters.sum {|l| TILES[l] }
   end
 
   def self.score(input)
