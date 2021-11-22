3627d9272890826fa581cf4b2563402024c47305
Kirsten <kcomandich@gmail.com>
2021-11-23 09:29:15 +1100

    Bowling: major refactor - add Frame class, rewrite to read more like the actual rules of the game.

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,59 +1,98 @@
+#require 'pry-byebug'
+
 class Game
-  attr_reader :frames, :points
+  attr_reader :frames
+
+  def initialize
+    @current_frame = 0
+    @bonus_score = []
+  end
 
   def ready_to_score?
-    frames.size > 9
+    if frames[9].strike?
+      @bonus_score.size > 1
+    elsif frames[8].strike?
+      @bonus_score.size > 0
+    elsif frames[9].spare?
+      @bonus_score.size > 0
+    else
+      frames[9].full?
+    end
   end
 
   def roll(pins)
-    raise BowlingError if pins < 0 || pins > 10
-    raise BowlingError if frames.last && frames.last.size == 1 && (frames.last[0] + pins > 10)
-    raise BowlingError if frames.size == 10 && frames.last[1] && (frames.last[0] + frames.last[1]) < 10
-    raise BowlingError if frames.size == 11 && frames[9][0] < 10 && frames.last[0] < 10
-    raise BowlingError if frames.size == 11 && frames[9][0] == 10 && frames.last[0] < 10 && frames.last[1]
+    raise BowlingError if ready_to_score? 
 
-    if pins == 10
-      frames << [pins, 0]
-    elsif (frames.last && frames.last.size == 2)
-      frames << [pins]
-    elsif frames.last && frames.last[0] != 10
-      frames.last << pins
+    if @current_frame < 10
+      frames[@current_frame] << pins
+
+      if frames[@current_frame].full?
+        @current_frame += 1
+      end
     else
-      frames << [pins]
+      raise BowlingError if pins < 0 || pins > 10
+      raise BowlingError if @bonus_score.size > 1
+      if @bonus_score[0]
+        raise BowlingError unless @bonus_score[0] == 10 || @bonus_score[0] + pins <= 10
+      end
+
+      @bonus_score << pins
     end
   end
 
   def score
     raise BowlingError unless ready_to_score? 
-    raise BowlingError if frames.size == 10 && (frames.last[0] + frames.last[1]) == 10  # bonus roll(s) missing
-    raise BowlingError if frames.size == 11 && frames[frames.size - 2][0] == 10 && frames.last[0] == 10  # final bonus roll missing
 
     sum = 0
-    frames.each.with_index do |f, i|
-      break if i == 10  # the bonus roll(s) shouldn't get added in again
-      unless f[1]
-        f[1] = 0   # should only need this if a single one gets added at the end?
-      end
-
-      sum += f[0] + f[1]
-
-      if f[0] == 10
-        sum += frames[i+1][0] + frames[i+1][1]
-        if frames[i+1][0] == 10
-          sum += frames[i+2][0]
-        end
-      elsif f[0] + f[1] == 10
-        sum += frames[i+1][0]
-      end
+    frames.each.with_index do |f,i|
+      sum += f.score if i < 10 # don't count the bonus rolls as frames of their own
+      sum += frames[i+1].rolls[0] if f.spare? && frames[i+1]
+      sum += frames[i+1].score if f.strike? && frames[i+1]
+      sum += frames[i+2].rolls[0] if f.strike? && frames[i+1] && frames[i+1].strike? && frames[i+2]
     end
-
-    sum
+    
+    sum += @bonus_score[0] if frames[8].strike?
+    sum += @bonus_score.sum
   end
 
   private
 
   def frames
-    @frames ||= []
+    @frames ||= Array.new(10) { Frame.new }
+  end
+
+  class Frame
+    def size
+      rolls.size
+    end
+
+    def << pins
+      raise BowlingError if pins < 0 || pins > 10
+      raise BowlingError if score + pins > 10
+
+      rolls << pins
+      rolls << 0 if pins == 10 && !full?
+    end
+
+    def rolls
+      @rolls ||= []
+    end
+
+    def full?
+      rolls.size == 2
+    end
+
+    def strike?
+      rolls[0] == 10
+    end
+
+    def spare?
+      !strike? && rolls[0] && rolls[1] && (rolls[0] + rolls[1] == 10)
+    end
+
+    def score
+      rolls.sum
+    end
   end
 
   class BowlingError < StandardError; end
