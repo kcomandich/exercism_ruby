fe1c355082913981355872cf06e067ba369be816
Kirsten Comandich <kcomandich@gmail.com>
2026-01-18 15:45:07 -0800

    Space Age: refactor using define_method, after reviewing community solutions

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,6 +1,7 @@
 class SpaceAge
 
-  ORBITAL_PERIOD = {
+  PLANETS = {
+    earth: 1.0,
     mercury: 0.2408467,
     venus: 0.61519726,
     mars: 1.8808158,
@@ -10,48 +11,15 @@ class SpaceAge
     neptune: 164.79132,
   }
 
-  private
+  SECS_PER_EARTH_YEAR = (365.25 * 24 * 60 * 60)
 
-  def initialize(age_in_seconds)
-    @age_in_seconds = age_in_seconds
-    @earth_age = age_in_seconds / (365.25 * 24 * 60 * 60)
+  def initialize(seconds)
+    @earth_age = seconds / SECS_PER_EARTH_YEAR
   end
 
-  def calc_age(planet)
-    @earth_age / ORBITAL_PERIOD[planet]
-  end
-
-  public
-
-  def on_earth
-    @earth_age
-  end
-
-  def on_mercury
-    calc_age(:mercury)
-  end
-
-  def on_venus
-    calc_age(:venus)
-  end
-
-  def on_mars
-    calc_age(:mars)
-  end
-
-  def on_jupiter
-    calc_age(:jupiter)
-  end
-
-  def on_saturn
-    calc_age(:saturn)
-  end
-
-  def on_uranus
-    calc_age(:uranus)
-  end
-
-  def on_neptune
-    calc_age(:neptune)
+  PLANETS.each do |planet, orbital_period|
+    define_method "on_#{planet}" do
+      @earth_age / orbital_period
+    end
   end
 end
